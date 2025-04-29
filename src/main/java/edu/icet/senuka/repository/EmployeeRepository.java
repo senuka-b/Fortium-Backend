ba72e1010b05d29b992caea9460094c6292e9829
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-29 09:58:52 +0530

    Create employee repository

---
+++ src/main/java/edu/icet/senuka/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package edu.icet.senuka.repository;
+
+import edu.icet.senuka.entity.EmployeeEntity;
+import edu.icet.senuka.util.DepartmentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    List<EmployeeEntity> findAllByDepartment(DepartmentType departmentType);
+    List<EmployeeEntity> findAllByNameIgnoreCase(String name);
+    Optional<EmployeeEntity> findByEmailIgnoreCase(String email);
+}
